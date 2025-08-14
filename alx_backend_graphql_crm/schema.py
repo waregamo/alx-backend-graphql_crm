3878b3b74dfb09043e5572b4964df7306ba1107a
waregamo <waregamoses20@gmail.com>
2025-08-14 18:58:59 +0300

    Fix GraphQL schema

--- alx_backend_graphql_crm/schema.py
+++ alx_backend_graphql_crm/schema.py
@@ -1,11 +1,9 @@
 import graphene
 from crm.schema import Query as CRMQuery, Mutation as CRMMutation
 
-class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hello, GraphQL!")
-
 class Query(CRMQuery, graphene.ObjectType):
-    pass
+
+    hello = graphene.String(default_value="Hello, GraphQL!")
 
 class Mutation(CRMMutation, graphene.ObjectType):
     pass
