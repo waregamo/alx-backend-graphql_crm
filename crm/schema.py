013037dde0385fd4594ad68f8c180ce83f3d8fb0
waregamo <waregamoses20@gmail.com>
2025-08-25 13:53:43 +0300

    Fix: Move UpdateLowStockProducts above Mutation in schema.py

--- crm/schema.py
+++ crm/schema.py
@@ -216,13 +216,7 @@ class Query(graphene.ObjectType):
     all_products = DjangoFilterConnectionField(ProductType)
     all_orders = DjangoFilterConnectionField(OrderType)
 
-# Mutation class wiring up
-class Mutation(graphene.ObjectType):
-    create_customer = CreateCustomer.Field()
-    bulk_create_customers = BulkCreateCustomers.Field()
-    create_product = CreateProduct.Field()
-    create_order = CreateOrder.Field()
-    update_low_stock_products = UpdateLowStockProducts.Field()
+
 
 
 
@@ -259,4 +253,11 @@ class UpdateLowStockProducts(graphene.Mutation):
                 message=f"Failed to update low stock: {str(e)}"
             )
   
+# Mutation class wiring up
+class Mutation(graphene.ObjectType):
+    create_customer = CreateCustomer.Field()
+    bulk_create_customers = BulkCreateCustomers.Field()
+    create_product = CreateProduct.Field()
+    create_order = CreateOrder.Field()
+    update_low_stock_products = UpdateLowStockProducts.Field()
 
